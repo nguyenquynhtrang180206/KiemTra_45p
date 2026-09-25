e728b69c83235291721dd91ddcd36c5b6f883c0a
Quynh Trang <nguyenquynhtrang180206@gmail.com>
2026-09-25 15:01:51 +0700

    Cap nhat products.php

--- Tu_luan/Cau2/products.php
+++ Tu_luan/Cau2/products.php
@@ -9,18 +9,13 @@ $stmt = $pdo->query("SELECT name, price, quantity FROM products");
 $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 //  2: Hiển thị thông tin của tất cả sản phẩm trong mảng
-echo "<h2>CỬA HÀNG JJ</h2>";
+echo "<h2>CỬA HÀNG ABC</h2>";
 echo "<h3>DANH SÁCH SẢN PHẨM</h3>";
-echo "<table border='1' cellpadding='8'>";
-echo "<tr><th>Tên sản phẩm</th><th>Giá</th><th>Số lượng</th></tr>";
 foreach ($products as $sp) {
-    echo "<tr>";
-    echo "<td>" . $sp['name'] . "</td>";
-    echo "<td>" . number_format($sp['price']) . "</td>";
-    echo "<td>" . $sp['quantity'] . "</td>";
-    echo "</tr>";
-}
-echo "</table>";
+    echo "Tên: " . $sp['name']
+       . " - Giá: " . number_format($sp['price'])
+       . " - Số lượng: " . $sp['quantity'] . "<br>";
+};
 
 //  3: Viết hàm để tính tổng giá trị tất cả sản phẩm (price * quantity)
 function calculateTotalValue($products) {
